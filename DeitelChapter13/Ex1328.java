c446d1a390ddd5b1a8cfb965695b72d719ff763a
shvnice <shvnice.git@gmail.com>
2018-11-27 19:28:32 +0100

    update

---
+++ DeitelChapter13/Ex1328.java
@@ -0,0 +1,18 @@
+/**
+13.28 (Selecting Shapes) Write an application that allows the user to select a shape from a JCombo- Box and draws 
+it 20 times with random locations and dimensions in method paintComponent. 
+The first item in the JComboBox should be the default shape that is displayed the first time paintCompo- nent is called.
+*/
+
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+
+public class Ex1328{
+
+}
+
+class SelectingShapes extends JFrame{
+	
+}
